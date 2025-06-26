e30fd31f3465a8953e944af9a39bb1dbae6b739f
rolitopdev <hectorj.24@hotmail.com>
2025-06-25 22:47:55 -0500

    Add astro-vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: vercel()
 });
 
 // Import dotenv config
